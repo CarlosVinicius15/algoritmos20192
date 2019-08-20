ca3302267b40107f4c17ff9b441ab901e089d27b
carlosviniciusagripino.sb@gmail.com <carlosviniciusagripino.sb@gmail.com>
2019-08-20 10:56:26 -0300

    Conversão de real em dolar

---
+++ Lista1ex05.py
@@ -0,0 +1,4 @@
+cota = float(input ("Informe o valor da cotação do dolar : "))
+quant = float(input ("Quanto em real deseja converter ? "))
+convert = quant / cota 
+print ("A quantidade em dolares obtida com os valores informados será de: ", convert, "US$" )
