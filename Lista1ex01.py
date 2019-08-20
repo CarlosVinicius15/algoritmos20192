79b64a6dfc67420390cafa7eb9c3442867cd68f2
CarlosVinicius15 <carlosviniciusagripino.sb@gmail.com>
2019-08-20 10:20:56 -0300

    Total de litros

--- Lista1ex01.py
+++ Lista1ex01.py
@@ -1,4 +1,4 @@
 litro_combustivel = float(input ("Informe o valor do litro de combustível: "))
 quant_dinheiro = float(input ("Informe quanto deseja gastar: "))
 total_litros = quant_dinheiro / litro_combustivel
-print("O total de litros obitidos com esses valores será {}.".format (total_litros))
+print("O total de litros obtidos com os valores informados será de {} litros.".format (total_litros)) 
