cd9b63cbcde7109cd5992068bbbd7831c5664a15
carlosviniciusagripino.sb@gmail.com <carlosviniciusagripino.sb@gmail.com>
2019-08-23 07:35:37 -0300

    Cálculo da idade de uma pessoa.

---
+++ Lista1ex09.py
@@ -0,0 +1,6 @@
+nome = str(input ("Qual o seu nome ? "))
+ano_nasci = int(input ("Em que ano nasceu ? "))
+ano_atual = int(input ("Em que ano estamos ? "))
+idade = ano_atual - ano_nasci
+print (nome, "tem", idade, "anos.")
+ 
