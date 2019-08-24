24ae18619789d4660ae4e6a27dcd58b4b5941b64
CarlosVinicius15 <carlosviniciusagripino.sb@gmail.com>
2019-08-24 18:21:57 -0300

    Total de carteiras em uma sala

---
+++ Lista1ex10.py
@@ -0,0 +1,9 @@
+Csala = float(input ("Informe o valor do comprimento da sala: "))
+Lsala = float(input ("Informe o valor da largura da sala: "))
+C_carteira = float(input ("Informe o valor do comprimento da carteira: "))
+L_carteira = float(input ("Informe o valor da largura da carteira: "))
+Areac = C_carteira * L_carteira
+total_l = int((Lsala + 0.5) / (L_carteira + 0.5))
+total_c = int((Csala - 1.5) + 0.2 / (C_carteira + 0.2))
+Total_sala = total_l * total_c
+print ("O total de carteiras na sala é ", Total_sala)
