f6263fb0e9faaea06e27b7918ac1b8eaddb3a815
carlosviniciusagripino.sb@gmail.com <carlosviniciusagripino.sb@gmail.com>
2019-08-26 10:05:07 -0300

    Verificação de sexo

---
+++ Lista2/Lista2ex01.py
@@ -0,0 +1,8 @@
+sexo = str(input ("Digite F para feminino e M para masculino: ").upper())
+
+if sexo == "F":
+    print ("Sexo feminino")
+elif sexo == "M":
+    print ("Sexo masculino")
+else:
+    print ("Sexo inválido")        
