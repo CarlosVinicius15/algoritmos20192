af07fd26cc162bdf7cde2ac581c1f14d92415941
carlosviniciusagripino.sb@gmail.com <carlosviniciusagripino.sb@gmail.com>
2019-08-26 10:10:43 -0300

    Verificar se a letra é vogal ou consoante

---
+++ Lista2/Lista2ex02.py
@@ -0,0 +1,7 @@
+letra = str(input ("Digite uma letra do alfabeto: ").upper())
+
+if letra == "A" or letra == "E" or letra == "I" or letra == "O" or letra == "U" :
+    print ("A letra é uma vogal")
+else:
+    print ("A letra é uma consoante")
+        
