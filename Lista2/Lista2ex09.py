f8b67c9226524e5c3ab53e9d53b7f3bd8ec03288
carlosviniciusagripino.sb@gmail.com <carlosviniciusagripino.sb@gmail.com>
2019-08-31 09:39:48 -0300

    Exibir dia da semana

---
+++ Lista2/Lista2ex09.py
@@ -0,0 +1,18 @@
+numero = int (input ("Digite um número de 1 até 7: ")) 
+
+if numero == 1 :
+    print ("1-Domingo")
+elif numero == 2 :
+    print ("2-Segunda")
+elif numero == 3 :
+    print ("3-Terça")
+elif numero == 4 :
+    print ("4-Quarta")
+elif numero == 5 :
+    print ("5-Quinta")
+elif numero == 6 :
+    print ("6-Sexta")
+elif numero == 7 :
+    print ("7-Sabado")
+else:
+    print ("Valor inválido")
