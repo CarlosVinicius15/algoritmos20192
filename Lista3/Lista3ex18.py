9ddf61148d8f93b84d94421a8908e9ed5521a4ee
CarlosVinicius15 <carlosviniciusagripino.sb@gmail.com>
2019-09-12 09:14:43 -0300

    Investimento em uma coleção de CDs

---
+++ Lista3/Lista3ex18.py
@@ -0,0 +1,8 @@
+quantCD = int(input("Digite a quantidade de CDs: "))
+valorTotal = 0
+for i in range(quantCD):
+	valorCD = eval(input("Digite o valor do CD: "))
+	valorTotal = valorTotal + valorCD
+	valorMedio = valorTotal / quantCD
+print("O valor total gasto foi de: R$", valorTotal)
+print("O valor médio gasto por cada CD foi de: R$ %.2f" % valorMedio)
