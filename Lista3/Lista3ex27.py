959de2d95bbdb737d912e9af05f622e24df1f0de
CarlosVinicius15 <carlosviniciusagripino.sb@gmail.com>
2019-09-14 13:20:02 -0300

    Calcular o valor de H com N termos.

---
+++ Lista3/Lista3ex27.py
@@ -0,0 +1,9 @@
+cont = 0
+soma = 0
+N = int(input ("Digite um número: "))
+while cont < N:
+	cont = cont + 1
+	H= (1/cont)
+	print (H)
+	soma = soma + H
+print ("O resultado total será: %.2f" %soma)
