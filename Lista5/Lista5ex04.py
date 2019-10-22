dbe14e80a44420cac4f35e90f512a16515af3ac1
CarlosVinicius15 <carlosviniciusagripino.sb@gmail.com>
2019-10-22 12:20:03 -0300

    Maior valor de um vetor.

---
+++ Lista5/Lista5ex04.py
@@ -0,0 +1,11 @@
+maior = 0
+cont = 0
+vet = []
+for i in range (0,10):
+	valor = int(input("Digite um valor: "))
+	vet.append(valor)
+for i in range (0,10):
+	if vet[i] > maior:
+		cont = cont + 1
+		maior = vet[i]
+print ("O maior elemento é: %d \nSua posição é: %d" %(maior, cont))
