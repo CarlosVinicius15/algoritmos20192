709d1d3ae18153b4022838b21ce6b89a93176ba8
Geraldo-Pereira <53914577+Geraldo-Pereira@users.noreply.github.com>
2019-11-11 15:32:28 -0300

    Update Lista5ex03.py

--- Lista5/Lista5ex03.py
+++ Lista5/Lista5ex03.py
@@ -2,7 +2,7 @@ vetor = [10]
 cont = 0
 for i in range (0,10):
 	valor = int(input ("Digite 10 valores: "))
-	vetor.append(valor)
+	vetor.append(valor)  #Não é permitido a utilização de funções de listas
 	if valor % 2 == 0:
 		cont = cont + 1
 print ("Existem %d valores pares" %cont)
